c6d6634315b0d8be6c01ccc8ecbf9a98609970e3
MKS-students <brianlbeeler@gmail.com>
2015-08-09 15:57:08 -0500

    Refactor

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,42 +4,49 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // Stringy will be the resultant stringified obj;
-  var stringy=""
+    var stringy="";
+    // Non-objects
+    if (typeof(obj)!=="object" || obj===null) {
+      stringy+=(""+obj===obj ?'"'+obj+'"': obj)
+    }
+    else {
+    // Arrays
+      if (Array.isArray(obj)) {
 
-  // Converts non-objects to appropriate string form
-  if (typeof(obj)!=="object" || obj===null) {
-  	stringy+=(""+obj===obj ?'"'+obj+'"': obj)
-  }  
-  else {
-  	// Stringifies arrays including contents recursively
-	if (Array.isArray(obj)) {
-  		stringy+="["
-		for (var i=0;i<obj.length;i++) {
-			stringy += stringifyJSON(obj[i])+",";  		}
- 		if (stringy[stringy.length-1]===",") { 
-			stringy=stringy.substring(0,stringy.length-1); 
-		}
-		stringy+="]"; 
-	} 
-  
-  	// Stringifies keys and properties recursively
-  	else {
-  		stringy+="{"
-  		for (var key in obj) {
-  			if ( typeof obj[key] !== "function" && typeof obj[key]!== "undefined") {
-  			stringy+=stringifyJSON(key)+":"+stringifyJSON(obj[key])+","
-  			}
-  		}
-  		if (stringy[stringy.length-1]===",") { 
-			stringy=stringy.substring(0,stringy.length-1); 
-		}
-		stringy+="}"; 
-  	}
+        stringy+="["
+
+        for (var i=0;i<obj.length;i++) {
+          stringy += stringifyJSON(obj[i])+",";
+        }
+
+        if (stringy[stringy.length-1]===",") {
+          stringy=stringy.substring(0,stringy.length-1);
+        }
+
+        stringy+="]";
+      }
+    // Keys and Properties
+    else {
+
+      stringy+="{"
+
+      for (var key in obj) {
+
+        if ( typeof obj[key] !== "function" && typeof obj[key]!== "undefined") {
+          stringy+=stringifyJSON(key)+":"+stringifyJSON(obj[key])+","
+        }
+
+      }
+
+      if (stringy[stringy.length-1]===",") {
+        stringy=stringy.substring(0,stringy.length-1);
+      }
+
+      stringy+="}";
+    }
   }
-
-  
-// Base case occurs when all loops have ran 
+// Base case -- all loops have run
+// Have arrived at stingified component
 return stringy
 }
 
